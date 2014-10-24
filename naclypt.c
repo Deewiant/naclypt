cceb5e37a84efac2a9194b9b377face32e456e02
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2014-10-24 21:08:52 +0300

    mlockall() right off to avoid secrets swapping out

--- naclypt.c
+++ naclypt.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -60,6 +61,11 @@ static void __attribute__ ((cold))
 }
 
 int main(int argc, char **argv) {
+   if (mlockall(MCL_CURRENT | MCL_FUTURE)) {
+      perror("Couldn't mlockall");
+      return 5;
+   }
+
    if (argc < 2 | argc > 3) {
       fprintf(stderr,
               "Usage: %s key [-d]\n"
